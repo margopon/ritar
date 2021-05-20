e1cc1760d2fb8dd5321d4e5996589c2d2940ca96
bogdan5522 <kvantorium7@gmail.com>
2021-05-20 19:40:39 +0700

    gg

--- app/src/main/java/com/example/roll_the_dice/MainActivity.kt
+++ app/src/main/java/com/example/roll_the_dice/MainActivity.kt
@@ -4,8 +4,8 @@ import android.media.AudioManager
 import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     private var soundId4 = 0
     private var soundId5 = 0
     private var soundId6 = 0
+    var Count = 0
 
 
     var play_sound_bufffer = 0
@@ -30,19 +31,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+
+
         findViewById<ImageView>(R.id.center_image).setOnClickListener {
             MainScope().launch {
+
 
                 var Random_Number = Random.nextInt(1..6)
                 var rotateDirection = arrayListOf(-1, 1).random()
                 var random_Buffer_XY = Random.nextBoolean()
                 var rotation_Time: Long = 1
-                var numberOfRotations_begin = Random.nextInt(1..2)
-                var numberOfRotations_end = Random.nextInt(5..7)
                 var Dice_scale: Float = 0.001F
 
 
-                for (i in numberOfRotations_begin..numberOfRotations_end) {
+                for (i in 1..4) {
 
                     Random_Number = Random.nextInt(1..6)
                     rotateDirection = arrayListOf(-1, 1).random()
@@ -66,12 +70,12 @@ class MainActivity : AppCompatActivity() {
 
                     findViewById<ImageView>(R.id.center_image).setImageResource(
                         when (Random_Number) {
-                            1 -> R.drawable.child
-                            2 -> R.drawable.momo
-                            3 -> R.drawable.drum
-                            4 -> R.drawable.trumpet
-                            5 -> R.drawable.waterfall
-                            6 -> R.drawable.wind
+                            1 -> R.drawable.one
+                            2 -> R.drawable.two
+                            3 -> R.drawable.three
+                            4 -> R.drawable.four
+                            5 -> R.drawable.five
+                            6 -> R.drawable.six
                             else -> R.drawable.ic_launcher_foreground
                         }
                     )
@@ -94,83 +98,18 @@ class MainActivity : AppCompatActivity() {
                 }
                 play_sound_bufffer = Random_Number
                 playSound(Random_Number)
-            }
+                Count = Count + Random_Number
+                score.text="Count: $Count"}
         }
 
-        findViewById<ImageView>(R.id.center_image2).setOnClickListener {
-            MainScope().launch {
-
-                var Random_Number = Random.nextInt(1..6)
-                var rotateDirection = arrayListOf(-1, 1).random()
-                var random_Buffer_XY = Random.nextBoolean()
-                var rotation_Time: Long = 1
-                var numberOfRotations_begin = Random.nextInt(1..2)
-                var numberOfRotations_end = Random.nextInt(5..7)
-                var Dice_scale: Float = 0.001F
-
-
-                for (i in numberOfRotations_begin..numberOfRotations_end) {
-
-                    Random_Number = Random.nextInt(1..6)
-                    rotateDirection = arrayListOf(-1, 1).random()
-                    random_Buffer_XY = Random.nextBoolean()
-
-
-                    if (random_Buffer_XY) {
-                        for (i in 0..89) {
-                            it.rotationX = it.rotationX + rotateDirection
-                            it.scaleX = it.scaleX + Dice_scale
-                            it.scaleY = it.scaleY + Dice_scale
-                            delay(rotation_Time)}}
-                    else {
-                        for (i in 0..89) {
-                            it.rotationY = it.rotationY + rotateDirection
-                            it.scaleX = it.scaleX + Dice_scale
-                            it.scaleY = it.scaleY + Dice_scale
-                            delay(rotation_Time)}}
-
-
-
-                    findViewById<ImageView>(R.id.center_image2).setImageResource(
-                        when (Random_Number) {
-                            1 -> R.drawable.child
-                            2 -> R.drawable.momo
-                            3 -> R.drawable.drum
-                            4 -> R.drawable.trumpet
-                            5 -> R.drawable.waterfall
-                            6 -> R.drawable.wind
-                            else -> R.drawable.ic_launcher_foreground
-                        }
-                    )
-                    if (random_Buffer_XY) {
-                        for (i in 90..179) {
-                            it.rotationX = it.rotationX + rotateDirection
-                            it.scaleX = it.scaleX - Dice_scale
-                            it.scaleY = it.scaleY - Dice_scale
-                            delay(rotation_Time)
-                        }
-
-                    } else {
-                        for (i in 90..179) {
-                            it.rotationY = it.rotationY + rotateDirection
-                            it.scaleX = it.scaleX - Dice_scale
-                            it.scaleY = it.scaleY - Dice_scale
-                            delay(rotation_Time)
-                        }
-                    }
-                }
-                play_sound_bufffer = Random_Number
-                playSound(Random_Number)
-            }
-        }
 
         soundPool = SoundPool(1, AudioManager.STREAM_MUSIC, 0)
-        soundId1 = soundPool!!.load(baseContext, R.raw.sound_child, 1)
-        soundId2 = soundPool!!.load(baseContext, R.raw.sound_momo, 1)
-        soundId3 = soundPool!!.load(baseContext, R.raw.sound_drum, 1)
-        soundId4 = soundPool!!.load(baseContext, R.raw.sound_trumpet, 1)
-        soundId5 = soundPool!!.load(baseContext, R.raw.sound_waterfall, 1)
-        soundId6 = soundPool!!.load(baseContext, R.raw.sound_wind, 1)
+        soundId1 = soundPool!!.load(baseContext, R.raw.one, 1)
+        soundId2 = soundPool!!.load(baseContext, R.raw.two, 1)
+        soundId3 = soundPool!!.load(baseContext, R.raw.three, 1)
+        soundId4 = soundPool!!.load(baseContext, R.raw.four, 1)
+        soundId5 = soundPool!!.load(baseContext, R.raw.five, 1)
+        soundId6 = soundPool!!.load(baseContext, R.raw.six, 1)
 
 
 
